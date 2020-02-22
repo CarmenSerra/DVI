2fe4840de72ec9e091fed3148da0f13513638fbf
CarmenSerrano <cserra02@ucm.es>
2020-02-22 12:41:18 +0100

    Cambio icono. Intentando background.

--- game.js
+++ game.js
@@ -2,10 +2,12 @@ export default class Game extends Phaser.Scene {
   constructor() {
     super({ key: 'main' });
   }
-  preload() {  
+  preload() {  //Aqui se precargan las imagenes para el juego, para que vaya mas rapido.
+    this.load.image('background','/possible_background_1.png');
   }
 
-  create() {
+  create() { //Este se ejecuta justo despues del preload.
+    this.background = this.game.add.sprite(0,0,'background');
   }
 
   update(time, delta) {    

ae66d682e1c8405216ab6a87a29b02003cdfc8dc
CarmenSerrano <cserra02@ucm.es>
2020-04-08 14:18:43 +0200

    Añadiendo música y continuando con los módulos
    Añadida diferente música para cuando gana vida, y para las diferentes armas.

--- docs/src/js/background_parallax.js
+++ docs/src/js/background_parallax.js
@@ -6,10 +6,46 @@ export default class Base extends Phaser.GameObjects.Sprite {
       this.scene.physics.add.existing(this, true);
       baseGroup.add(this);
       this.y -= this.height / 2 + platform.height / 2;
-  
 
-  
     }
+
+
+    create(){
+            this.war1_sky = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'war1_sky');
+            this.war1_sky.setOrigin(0, 0);
+            this.war1_sky.setScrollFactor(0);
+
+            this.war1_sun = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'war1_sun');
+            this.war1_sun.setOrigin(0, 0);
+            this.war1_sun.setScrollFactor(0);
+
+            this.war1_ruins = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'war1_ruins');
+            this.war1_ruins.setOrigin(0, 0);
+            this.war1_ruins.setScrollFactor(0);
+
+            this.war1_house3 = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'war1_house3');
+            this.war1_house3.setOrigin(0, 0);
+            this.war1_house3.setScrollFactor(.4);
+
+            this.war1_house2 = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'war1_house2');
+            this.war1_house2.setOrigin(0, 0);
+            this.war1_house2.setScrollFactor(.5);
+
+            this.war1_house1 = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'war1_house1');
+            this.war1_house1.setOrigin(0, 0);
+            this.war1_house1.setScrollFactor(.6);
+
+            this.war1_fence = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'war1_fence');
+            this.war1_fence.setOrigin(0, 0);
+            this.war1_fence.setScrollFactor(.7);
+
+            this.war1_road = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'war1_road');
+            this.war1_road.setOrigin(0, 0);
+            this.war1_road.setScrollFactor(0);
+
+
+    }
+    //PUEDE QUE EL SPAWN SEA EN EL GAME.JS NUEVO
     spawn() {
       this.scene.add.existing(new Star(this.scene, this, this.x, this.y));
     }
